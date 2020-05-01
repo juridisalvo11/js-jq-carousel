1e230d37e9543096e73a5d4aae92687bc9cea856
Juri Di salvo <Juridisalvo11@gmail.com>
2020-05-01 19:19:23 +0200

    Carousel

--- sheet.js
+++ sheet.js
@@ -3,36 +3,42 @@
 //Intercetto il click sul tasto next e Recupero l'immagina con la classe visible
 $('.next').click(function() {
     var img_attuale = $('img.visible');
-    console.log(img_attuale);
+    var selettore_foto = $('i.visible');
     //Rimuovo la classe visible dall'immagine corrente e l'aggiungo alla successiva
     img_attuale.removeClass('visible');
+    selettore_foto.removeClass('visible');
     //Recupero l'immagine successiva con il comando jquery next che mi permette di prendere il fratello immediatamente successivo dell'img img_attuale
     var img_successiva = img_attuale.next('img');
-    console.log(img_successiva);
+    var selettore_successivo = selettore_foto.next('i')
     //Verifico se c'è un'immagine successiva e vado ad aggingerle la classe
     if (img_successiva.length != 0) {
     img_successiva.addClass('visible');
+    selettore_successivo.addClass('visible');
 } else {
     //Reimposto la classe visible alla prima immagine
     $('img:first-child').addClass('visible');
+    $('.first-selector').addClass('visible');
 }
 
 })
 
 $('.prev').click(function() {
     var img_attuale = $('img.visible');
-    console.log(img_attuale);
+    var selettore_foto = $('i.visible');
     //Rimuovo la classe visible dall'immagine corrente e l'aggiungo alla successiva
     img_attuale.removeClass('visible');
+    selettore_foto.removeClass('visible');
     //Recupero l'immagine successiva con il comando jquery next che mi permette di prendere il fratello immediatamente successivo dell'img img_attuale
     var img_precedente = img_attuale.prev('img');
-    console.log(img_precedente);
+    var selettore_successivo = selettore_foto.prev('i')
     //Verifico se c'è un'immagine successiva e vado ad aggingerle la classe
     if (img_precedente.length != 0) {
     img_precedente.addClass('visible');
+    selettore_successivo.addClass('visible');
 } else {
     //Reimposto la classe visible alla prima immagine
     $('img:last-child').addClass('visible');
+    $('.last-selector').addClass('visible');
 }
 
 })
